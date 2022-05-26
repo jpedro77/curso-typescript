1a484a779ae5c8387ee5f67905b933c2b031f385
Joao Pedro <jpedrofernandes77@gmail.com>
2022-05-25 21:58:30 -0300

    [006] Capitulo sobre genericos

---
+++ genericos/genericos.js
@@ -0,0 +1,20 @@
+"use strict";
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo('João').length);
+console.log(echo(27).length);
+console.log(echo({ nome: 'João', idade: 27 }));
+// Usando Generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('João').length);
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: 'João', idade: 27 }));
+// Generics desponíveis na API
+const avaliacoes = [10, 9.3, 7.7];
+avaliacoes.push(8.4);
+// avaliacoes.push('5.5')
+console.log(avaliacoes);
+//# sourceMappingURL=genericos.js.map
